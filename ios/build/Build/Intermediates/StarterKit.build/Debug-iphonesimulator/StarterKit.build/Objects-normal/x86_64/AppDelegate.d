ac6b2f03fe6ef1af8c3b95c9915b5103540769b7
Zarmeen <zarmeennasim@yahoo.com>
2017-02-08 16:26:41 +0500

    code merged

--- ios/build/Build/Intermediates/StarterKit.build/Debug-iphonesimulator/StarterKit.build/Objects-normal/x86_64/AppDelegate.d
+++ ios/build/Build/Intermediates/StarterKit.build/Debug-iphonesimulator/StarterKit.build/Objects-normal/x86_64/AppDelegate.d
@@ -9,4 +9,6 @@ dependencies: \
   ../node_modules/react-native/React/Base/RCTDefines.h \
   ../node_modules/react-native/React/Base/RCTBridgeModule.h \
   ../node_modules/react-native/React/Base/RCTFrameUpdate.h \
-  ../node_modules/react-native/React/Base/RCTInvalidating.h
+  ../node_modules/react-native/React/Base/RCTInvalidating.h \
+  ../node_modules/react-native-azurenotificationhub/ios/RCTAzureNotificationHubManager/RCTAzureNotificationHubManager.h \
+  ../node_modules/react-native/React/Modules/RCTEventEmitter.h
