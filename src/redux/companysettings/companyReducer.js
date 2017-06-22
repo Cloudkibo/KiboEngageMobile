74661ac8f869207267bd5b4a102d7fd2eea7757b
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-06-22 23:29:06 +0500

    Company Settings Status

--- src/redux/companysettings/companyReducer.js
+++ src/redux/companysettings/companyReducer.js
@@ -11,10 +11,12 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
    
    case ActionTypes.FETCH_SETTINGS:
-      return {...state,data:[action.payload]};
+      return {...state,data:[action.payload], updateSettings: ''};
     
    case ActionTypes.UPDATE_SETTINGS:
       return {...state,updateSettings:action.payload};
+  case ActionTypes.RESET_STATUS:
+      return {...state,updateSettings:''};
   
    
     default:
