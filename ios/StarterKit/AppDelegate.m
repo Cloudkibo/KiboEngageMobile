9735308b65192097a71814f15b04aba4110396a2
sumairasaeed <sumaira.syedsaeed@gmail.com>
2017-07-15 16:39:56 +0500

    fixes merge errors

--- ios/StarterKit/AppDelegate.m
+++ ios/StarterKit/AppDelegate.m
@@ -23,7 +23,8 @@
 #ifdef DEBUG
     jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 #else
-   jsCodeLocation = [CodePush bundleURL];
+   //jsCodeLocation = [CodePush bundleURL];
+   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
  
 #endif
 
